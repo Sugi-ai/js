220bb49f24750f96a375aa703783067920396038
Sugi-ai <suhdei90@gmail.com>
2025-07-08 19:46:58 +0800

    js 3input max olson

--- dom2.js
+++ dom2.js
@@ -3,9 +3,24 @@ let num2Input = document.getElementById("num2-input");
 let num3Input = document.getElementById("num3-input");
 let maxbutton = document.getElementById("max");
 
-multiplybutton.addEventListener("click", () => {
+maxbutton.addEventListener("click", () => {
   let num1 = parseInt(num1Input.value);
   let num2 = parseInt(num2Input.value);
   let num3 = parseInt(num3Input.value);
-  console.log(parseInt(num5.value) * parseInt(num6.value));
+  
+
+    if (num1 > num2) {
+        if (num3 > num1) {
+            console.log(num3);
+        } else {
+            console.log(num1);
+        }
+        
+    } else {
+        if (num2 > num3) {
+            console.log(num2);
+        } else {
+            console.log (num3);
+        }
+    }
 });
