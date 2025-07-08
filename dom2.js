232f7e7abfbf5d1282c5a1d35694c1ca5596897f
Sugi-ai <suhdei90@gmail.com>
2025-07-08 21:58:05 +0800

    middle num oloh

--- dom2.js
+++ dom2.js
@@ -24,3 +24,43 @@ maxbutton.addEventListener("click", () => {
         }
     }
 });
+
+        // middle  //
+
+let number1Input = document.getElementById("num1-input");
+let number2Input = document.getElementById("num2-input");
+let number3Input = document.getElementById("num3-input");
+let middlebutton = document.getElementById("middle");
+
+middlebutton.addEventListener("click", () => {
+  let num1 = parseInt(number1Input.value);
+  let num2 = parseInt(number2Input.value);
+  let num3 = parseInt(number3Input.value);
+
+//     let middle;
+
+//     if ((num1 > num2 && num1 < num3) || (num1 < num2 && num1 > num3)) {
+//         consol.log(num1);
+//     } else if ((num2 > num1 && num2 < num3) || (num2 < num1 && num2 > num3)) {
+//         consol.log(num2);
+//     } else {
+//         consol.log(num3);
+//     }
+//         console.log (middle);
+// });
+
+ if (num1 > num2) {
+        if (num3 < num2) {
+            console.log(num2);
+        } else {
+            console.log(num3);
+        }
+        
+    } else {
+        if (num2 < num3) {
+            console.log(num2);
+        } else {
+            console.log (num3);
+        }
+    }
+});
